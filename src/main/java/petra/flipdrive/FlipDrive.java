ac8ba7088c5308c78c6bde67875152c201bdb7ef
Petra Kupinski <89861450+PhantasmaCora@users.noreply.github.com>
2022-08-18 14:29:21 -0500

    initialize

---
+++ src/main/java/petra/flipdrive/FlipDrive.java
@@ -0,0 +1,27 @@
+package petra.flipdrive;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import com.google.common.collect.ImmutableSet;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FlipDrive implements ModInitializer {
+	public static final String MODID = "flipdrive";
+	
+	// ########### BLOCKS ############
+	
+	
+	// ########### INIT ############
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+	
+
+
+}
